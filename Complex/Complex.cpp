da7831af3bf037c9066e1ed67cd8d7a0c1703285
KulikovaDaria <dariya.kulikova@gmail.com>
2018-09-16 20:59:42 +0300

    add Complex

---
+++ Complex/Complex.cpp
@@ -0,0 +1,155 @@
+#include "Complex.h"
+#include <iostream>
+
+Complex::Complex(const double real)
+	:Complex(real, 0.0)
+{
+}
+
+Complex::Complex(const double real, const double imaginary)
+	: re(real)
+	,im(imaginary)
+{
+}
+
+bool Complex::operator==(const Complex& rhs) const
+{
+	double eps(0.000001);
+	return (abs(re - rhs.re) < eps) && (abs(im - rhs.im) < eps);
+}
+
+Complex& Complex::operator+=(const Complex& rhs)
+{
+	re += rhs.re;
+	im += rhs.im;
+	return *this;
+}
+
+Complex operator+(const Complex& lhs, const Complex& rhs)
+{
+	Complex sum(lhs);
+	sum += rhs;
+	return sum;
+}
+
+Complex operator+(const Complex& lhs, const double rhs)
+{
+	Complex sum(lhs + Complex(rhs));
+	return sum;
+}
+
+Complex operator+(const double lhs, const Complex& rhs)
+{
+	Complex sum(Complex(lhs) + rhs);
+	return sum;
+}
+
+Complex& Complex::operator-=(const Complex& rhs)
+{
+	re -= rhs.re;
+	im -= rhs.im;
+	return *this;
+}
+
+Complex operator-(const Complex& lhs, const Complex& rhs)
+{
+	Complex sum(lhs);
+	sum -= rhs;
+	return sum;
+}
+
+Complex operator-(const Complex& lhs, const double rhs)
+{
+	Complex sum(lhs - Complex(rhs));
+	return sum;
+}
+
+Complex operator- (const double lhs, const Complex& rhs)
+{
+	Complex sum(Complex(lhs) - rhs);
+	return sum;
+}
+
+Complex& Complex::operator*=(const Complex& rhs)
+{
+	Complex sum;
+	sum.re = re * rhs.re - im * rhs.im;
+	sum.im = re * rhs.im + rhs.re*im;
+	*this = sum;
+	return *this;
+}
+
+Complex operator*(const Complex& lhs, const Complex& rhs)
+{
+	Complex sum(lhs);
+	sum *= rhs;
+	return sum;
+}
+
+Complex operator*(const Complex& lhs, const double rhs)
+{
+	Complex sum(lhs*Complex(rhs));
+	return sum;
+}
+
+Complex operator*(const double lhs, const Complex& rhs)
+{
+	Complex sum(Complex(lhs)*rhs);
+	return sum;
+}
+
+Complex& Complex::operator/=(const Complex& rhs)
+{
+	Complex sum;
+	sum.re = (re*rhs.re + im * rhs.im) / (rhs.re*rhs.re + rhs.im*rhs.im);
+	sum.im = (rhs.re*im - re * rhs.im) / (rhs.re*rhs.re + rhs.im*rhs.im);
+	*this = sum;
+	return *this;
+}
+
+Complex operator/(const Complex& lhs, const Complex& rhs)
+{
+	Complex sum(lhs);
+	sum /= rhs;
+	return sum;
+}
+
+Complex operator/(const Complex& lhs, const double rhs)
+{
+	Complex sum(lhs / Complex(rhs));
+	return sum;
+}
+
+Complex operator/(const double lhs, const Complex& rhs)
+{
+	Complex sum(Complex(lhs) / rhs);
+	return sum;
+}
+
+std::ostream& Complex::writeTo(std::ostream& ostrm) const
+{
+	ostrm << leftBrace << re << separator << im << rightBrace;
+	return ostrm;
+}
+
+std::istream& Complex::readFrom(std::istream& istrm)
+{
+	char leftBrace(0);
+	double real(0.0);
+	char comma(0);
+	double imaginary(0.0);
+	char rightBrace(0);
+	istrm >> leftBrace >> real >> comma >> imaginary >> rightBrace;
+	if (istrm.good())
+	{
+		if ((Complex::leftBrace == leftBrace) && (Complex::separator == comma) && (Complex::rightBrace == rightBrace))
+		{
+			re = real;
+			im = imaginary;
+		}
+		else {
+			istrm.setstate(std::ios_base::failbit);
+		}
+	}
+	return istrm;
+}
---
+++ Complex/Complex_test.cpp
@@ -0,0 +1,78 @@
+#include "Complex.h"
+#include <iostream>
+#include <sstream>
+
+bool testParse(const std::string& str)
+{
+	using namespace std;
+	istringstream istrm(str);
+	Complex z;
+	istrm >> z;
+	if (istrm.good())
+	{
+		cout << "Read success: " << str << " -> " << z << endl;
+	}
+	else {
+		cout << "Read error: " << str << " -> " << z << endl;
+	}
+	return istrm.good();
+}
+
+int main()
+{
+	using namespace std;
+
+	testParse("{8.9,9}");
+	testParse("{8.9, 9}");
+	testParse("{8.9,9");
+	cout << endl;
+	cout << "Complex() = " << Complex() << "; Complex(8.9) = " <<
+		Complex(8.9) << "; Complex(8.9, -9.1) = " << Complex(8.9, -9.1) << endl;
+	Complex a(-8.9, 9.1);
+	Complex b(-8.9, 9.1);
+	Complex c(-8.9, 5.2);
+	cout << a << "==" << b << " -> " << (a == b) << ";  " << a << "==" << c << " -> " << (a == c) << endl;
+	cout << a << "!=" << b << " -> " << (a != b) << ";  " << a << "!=" << c << " -> " << (a != c) << endl;
+	cout << endl;
+	a = Complex(1.0, 1.1);
+	b = Complex(1.2, -2.2);
+	cout << "A = " << a << endl;
+	cout << "A += " << b << " -> " << (a += b) << endl;
+	cout << "A += 1.1 -> " << (a += 1.1) << endl;
+	cout << a << " + " << b << " = " << (a + b) << endl;
+	cout << a << " + 1.1 = " << (a + 1.1) << endl;
+	cout << "1.1 + " << a << " = " << (1.1 + a) << endl;
+	cout << endl;
+	a = Complex(5.8, 4.4);
+	b = Complex(1.4, 1.1);
+	cout << "A = " << a << endl;
+	cout << "A -= " << b << " -> " << (a -= b) << endl;
+	cout << "A -= 1.1 -> " << (a -= 1.1) << endl;
+	cout << a << " - " << b << " = " << (a - b) << endl;
+	cout << a << " - 1.1 = " << (a - 1.1) << endl;
+	cout << "8.8 - " << a << " = " << (8.8 - a) << endl;
+	cout << endl;
+	a = Complex(1.1, 2.0);
+	b = Complex(5.0, 3.0);
+	cout << "A = " << a << endl;
+	cout << "A *= " << b << " -> " << (a *= b) << endl;
+	cout << "A *= 2 -> " << (a *= 2) << endl;
+	cout << a << " * " << b << " = " << (a*b) << endl;
+	cout << a << " * 5 = " << (a * 5) << endl;
+	cout << "5 * " << a << " = " << (5 * a) << endl;
+	cout << endl;
+	a = Complex(32, 8);
+	b = Complex(1, 2);
+	cout << "A = " << a << endl;
+	cout << "A /= " << b << " -> " << (a /= b) << endl;
+	cout << "A /= 2 -> " << (a /= 2) << endl;
+	cout << a << " / " << b << " = " << (a / b) << endl;
+	cout << a << " / 2 = " << (a / 2) << endl;
+	cout << "64 / " << a << " = " << (64 / a) << endl;
+	cout << endl;
+
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
